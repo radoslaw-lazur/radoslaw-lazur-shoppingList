4101532f6602e6f585cdad424bce6e8d206cb60b
radoslaw-lazur <radoslaw.lazur@gmail.com>
2019-11-25 21:44:58 +0100

    ShoppingListCommit

---
+++ src/main/java/com/shoppinglist/domain/Product.java
@@ -0,0 +1,11 @@
+package com.shoppinglist.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class Product {
+    private Long id;
+    private String productName;
+}
